fcd0c062e8231f0e035e7d26bbcb254cfc8afb6a
Vladimir Abramichev <vladimir.abramichev@adyen.com>
2021-12-14 18:41:51 +0100

    Refactoring for plugin-like structure

--- APIClient.js
+++ APIClient.js
@@ -2,77 +2,44 @@
 import { environment } from "./Configuration";
   
 export const fetchPaymentMethods = (configuration) => {
-  let paymentMethodsRequest = new Request(environment.url + 'paymentMethods', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'X-API-Key': environment.apiKey
-  },
-  body: JSON.stringify({
-    merchantAccount: configuration.merchantAccount,
-    countryCode: configuration.countryCode,
-    shopperLocale: configuration.shopperLocale,
-    amount: configuration.amount,
-    channel: configuration.channel
-  })
-  });
+    let body = {
+      merchantAccount: configuration.merchantAccount,
+      countryCode: configuration.countryCode,
+      shopperLocale: configuration.shopperLocale,
+      amount: configuration.amount,
+      channel: configuration.channel
+    };
 
-  return fetch(paymentMethodsRequest)
-    .then(response => {
-      console.log(response.headers);
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        response.json().then( data => { console.log(data); } );
-        throw new Error('Payment methods error ' + response.status);
-      }
-    })
+    return fetchFrom(environment.url + 'paymentMethods', body)
 };
 
-export const fetchPayments = (paymentData) => {
-  let paymentRequest = new Request(environment.url + 'payments', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-API-Key': environment.apiKey
-    },
-    body: JSON.stringify(paymentData)
-  });
-
-  return fetch(paymentRequest)
-  .then(response => {
-    console.log(response.headers);
-    if (response.status === 200) {
-      return response.json();
-    } else {
-      response.json().then( data => { console.log(data); } );
-      throw new Error('Payments error ' + response.status );
-    }
-  })
+export const fetchPayments = (data) => {
+  return fetchFrom(environment.url + 'payments', data);
 };
   
-export const fetchPaymentDetails = (configuration, details, paymentData) => {
-  let paymentDetailsRequest = new Request(environment.url + 'payments/details', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-API-Key': environment.apiKey
-    },
-    body: JSON.stringify({
-      paymentData: paymentData,
-      details: details,
-      merchantAccount: configuration.merchantAccount
-    })
-  });
+export const fetchPaymentDetails = (data) => {
+  return fetchFrom(environment.url + 'payments/details', data);
+};
 
-return fetch(paymentDetailsRequest)
-  .then(response => {
-      console.log(response.headers);
-    if (response.status === 200) {
-      return response.json();
-    } else {
-      response.json().then( data => { console.log(data); } );
-      throw new Error('Payment details error ' + response.status);
-    }
-  })
+const fetchFrom = (url, body) => {
+  let paymentMethodsRequest = new Request(url, 
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': environment.apiKey
+      },
+      body: JSON.stringify(body)
+    });
+  
+    return fetch(paymentMethodsRequest)
+      .then(response => {
+        console.log(response.headers);
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          response.json().then( data => { console.log(data); } );
+          throw new Error('Network error ' + url + ': \n' + response.status);
+        }
+      })
 };
--- PaymentView.js
+++ PaymentView.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-import { AdyenDropInProvider, AdyenCheckoutContext } from './AdyenDropInProvider';
+import { AdyenPaymentProvider } from './AdyenPaymentProvider';
 import { channel } from './Configuration';
+import { fetchPayments, fetchPaymentDetails } from './APIClient' ;
+
+import { NativeModules } from 'react-native';
+const { AdyenDropIn } = NativeModules;
 
 import {
+  Alert,
   Button,
   SafeAreaView,
   StatusBar,
@@ -51,10 +56,53 @@ const PaymentView = () => {
     const onOpenDropInPress = (context) => {
       if (context.paymentMethods) {
         console.log('Using local payment methods');
-        openDropInComponent.apply(this, [context.paymentMethods, context.config]);
+        AdyenDropIn.openDropIn(context.paymentMethods, context.config);
       } else {
         console.log('No payment methods!');
       }
+    };
+
+    const didSubmit = (data) => {
+      console.log('didSubmit called: ', data.paymentMethod.type);
+    
+      fetchPayments(data)
+      .then(result => {
+        if (result.action) {
+          console.log('- - Action!');
+          AdyenDropIn.handle(result.action);
+        } else {
+          console.log('- - Not en action!');
+          console.log(result);
+          Alert.alert('Payment', result.resultCode);
+        }
+      })
+      .catch(error => {
+        console.log('Network error:', error);
+      })
+    };
+    
+    const didProvide = (data) => {
+      console.log('didProvide called '), data;
+      AdyenDropIn.hideDropIn();
+    
+      fetchPaymentDetails(data)
+      .then(result => {
+          Alert.alert('Payment', result.resultCode);
+      })
+      .catch(error => {
+        console.log('Network error:', error);
+      })
+    };
+    
+    const didComplete = () => {
+      console.log('didComplete called');
+      AdyenDropIn.hideDropIn();
+    };
+    
+    const didFail = (error) => {
+      console.log('setDidFail called: ', error);
+      Alert.alert('Error', error.message);
+      AdyenDropIn.hideDropIn();
     };
   
     return (
@@ -67,32 +115,35 @@ const PaymentView = () => {
               <Text> {context.config.amount.value} {context.config.amount.currency}</Text>
               <Text> Country: {context.paymentMethods == null ? "❗️" : getFlagEmoji(context.config.countryCode)} </Text>
             </View>
-
-            <View
-              style={[
-                styles.contentView,
-                contentBackgroundStyle
-              ]}>
             
-            <AdyenDropInProvider>
-            { adyenCheckout => ( 
-              <Button
-                title="Open DropIn"
-                onPress={ () => { adyenCheckout.openDropInComponent(context.paymentMethods, context.config) } } />
-              )}
-            </AdyenDropInProvider>
+            <AdyenPaymentProvider
+              didSubmit={didSubmit}
+              didProvide={didProvide}
+              didFail={didFail}
+              didComplete={didComplete} >
+                { adyenPayment => ( 
+                  <View style={[ styles.contentView, contentBackgroundStyle ]}>
+                  <Button
+                    title="Open DropIn"
+                    onPress={ () => {
+                      onOpenDropInPress(context);
+                      adyenPayment.start(context.config);
+                    } } />
 
-            <Button
-              title="Open Card Component (WIP)"
-              disabled={true} />
-            <Button
-              title="Open iDEAL (WIP)"
-              disabled={true} />
-            <Button
-              title={platformSpecificButton}
-              disabled={true} />
+                  <Button
+                    title="Open Card Component (WIP)"
+                    disabled={true} />
+                  <Button
+                    title="Open iDEAL (WIP)"
+                    disabled={true} />
+                  <Button
+                    title={platformSpecificButton}
+                    disabled={true} />
+
+                </View>
+              )}
+              </AdyenPaymentProvider>
                 
-            </View>
           </SafeAreaView>
         )}
       </PaymentMethodsContext.Consumer>
