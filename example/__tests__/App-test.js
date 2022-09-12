d107489b1e5243b1a5d04e278eb4e815f7bd68ce
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2022-09-12 15:52:18 +0200

    chore: add Jest

--- example/__tests__/App-test.js
+++ example/__tests__/App-test.js
@@ -2,13 +2,45 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+// import { Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { render } from '@testing-library/react-native';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+export function renderWithNavigation(ui) {
+  return render(<NavigationContainer>{ui}</NavigationContainer>);
+}
+
+import PaymentMethods from '@adyen/react-native';
+import PaymentMethodsProvider from '@adyen/react-native';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderWithNavigation(<PaymentMethodsProvider>
+    <PaymentMethods />
+  </PaymentMethodsProvider> );
 });
+
+
+/* ]
+
+import { render, screen, fireEvent } from '@testing-library/react-native';
+import { QuestionsBoard } from '../QuestionsBoard';
+
+test('form submits two answers', () => {
+  const allQuestions = ['q1', 'q2'];
+  const mockFn = jest.fn();
+
+  render(<QuestionsBoard questions={allQuestions} onSubmit={mockFn} />);
+
+  const answerInputs = screen.getAllByLabelText('answer input');
+
+  fireEvent.changeText(answerInputs[0], 'a1');
+  fireEvent.changeText(answerInputs[1], 'a2');
+  fireEvent.press(screen.getByText('Submit'));
+
+  expect(mockFn).toBeCalledWith({
+    '1': { q: 'q1', a: 'a1' },
+    '2': { q: 'q2', a: 'a2' },
+  });
+});
+
+*/
