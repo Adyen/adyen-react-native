1af4484e33d28a9c4fb2d8fb06c0e2675a2b0294
vladimir <vladimir.abramichev@mail.ru>
2022-10-21 11:41:52 +0200

    chore: refactor JS constants

--- src/Constants.js
+++ src/Constants.js
@@ -2,3 +2,5 @@ export const PAYMENT_SUBMIT_EVENT = 'didSubmitCallback';
 export const PAYMENT_PROVIDE_DETAILS_EVENT = 'didProvideCallback';
 export const PAYMENT_COMPLETED_EVENT = 'didCompleteCallback';
 export const PAYMENT_FAILED_EVENT = 'didFailCallback';
+
+export const ERROR_CANCELED = 'canceledByShopper';
