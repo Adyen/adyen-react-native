abd0a9641b7aaafc17c2815dd597d5f8ccd3fec2
vladimir <vladimir.abramichev@mail.ru>
2022-10-24 18:10:58 +0200

    chore: remove on iOS

--- ios/AdyenModule.m
+++ ios/AdyenModule.m
@@ -20,18 +20,6 @@ RCT_EXTERN_METHOD(handle:(NSDictionary *)action)
 
 @end
 
-@interface RCT_EXTERN_MODULE(AdyenCardComponent, NSObject)
-
-RCT_EXTERN_METHOD(open:(NSDictionary *)paymentMethods
-                  configuration:(NSDictionary *)configuration)
-
-RCT_EXTERN_METHOD(hide:(nonnull NSNumber *)success
-                  event:(NSDictionary *)event)
-
-RCT_EXTERN_METHOD(handle:(NSDictionary *)action)
-
-@end
-
 @interface RCT_EXTERN_MODULE(AdyenInstant, NSObject)
 
 RCT_EXTERN_METHOD(open:(NSDictionary *)paymentMethods
@@ -61,4 +49,3 @@ RCT_EXTERN_METHOD(open:(NSDictionary *)paymentMethods
                 configuration:(NSDictionary *)configuration)
 
 @end
-
