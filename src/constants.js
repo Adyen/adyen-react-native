f92ab7e37e9a2a0fea82f7b2ce4ab4378bfa2f97
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2022-10-24 18:00:08 +0200

    chore: add folder

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const PAYMENT_SUBMIT_EVENT = 'didSubmitCallback';
+export const PAYMENT_PROVIDE_DETAILS_EVENT = 'didProvideCallback';
+export const PAYMENT_COMPLETED_EVENT = 'didCompleteCallback';
+export const PAYMENT_FAILED_EVENT = 'didFailCallback';
+
+export const ERROR_CODE_CANCELED = 'canceledByShopper';
+export const ERROR_CODE_NOT_SUPPORTED = 'notSupported';
+export const ERROR_CODE_NO_CLIENT_KEY = 'noClientKey';
+export const ERROR_CODE_NO_PAYMENT = 'noPayment';
+export const ERROR_CODE_INVALID_PAYMENT_METHODS = 'invalidPaymentMethods';
+export const ERROR_CODE_INVALID_ACTION = 'invalidAction';
+export const ERROR_CODE_NO_PAYMENT_METHOD = 'noPaymentMethod';
