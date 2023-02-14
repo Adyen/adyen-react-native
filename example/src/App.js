6bed224139dbd99325bd2d99b29b310c0da6200d
Vladimir Abramichev <2648655+descorp@users.noreply.github.com>
2023-02-14 17:23:05 +0100

    chore: apply theme for android

--- example/src/App.js
+++ example/src/App.js
@@ -7,14 +7,14 @@
  */
 
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SettingView from './SettingsView';
 import CheckoutView from './CheckoutView';
 import Result from './ResultView';
 import PaymentMethodsProvider from './PaymentMethodsProvider';
 
-import { Button, Alert, View } from 'react-native';
+import { Button, Alert, View, useColorScheme } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
@@ -30,19 +30,22 @@ const Home = ({ navigation }) => {
 };
 
 const App = () => {
+
+  const isDarkMode = useColorScheme() === 'dark';
+
   return (
     <PaymentMethodsProvider
       onError={(error) => {
         Alert.alert('Payment Methods', error.message || 'Error');
       }}
     >
-      <NavigationContainer>
+      <NavigationContainer theme={ isDarkMode ? DarkTheme : DefaultTheme }>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen
             name="CheckoutPage"
             component={CheckoutView}
-            options={({ navigation, route }) => ({
+            options={({ navigation }) => ({
               title: 'Adyen React Native',
               headerRight: () => (
                 <Button
