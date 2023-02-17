eb1efab659c3d5703728d90dde3a9e9a269a1aa8
Vladimir Abramichev <2648655+descorp@users.noreply.github.com>
2023-02-17 19:21:57 +0100

    chore: add Android styles

--- example/src/CSE.js
+++ example/src/CSE.js
@@ -2,25 +2,47 @@ import React from 'react';
 import { Button, SafeAreaView } from 'react-native';
 import { NativeModules } from 'react-native';
 import { ENVIRONMENT } from './Configuration';
+import Styles from './Styles';
 
 const { AdyenCSE } = NativeModules;
 
 const CseView = () => {
-  const tryEncrypt = () => {
+  const tryEncryptCard = async () => {
     let unencryptedCard = {
       number: '5454 5454 5454 5454',
-      expiration: '03/30',
-      cvc: '737',
+      expiryMonth: '03',
+      expiryYear: '2030',
+      cvv: '737',
     };
     let publicKey = ENVIRONMENT.publicKey;
-    AdyenCSE.encryptCard(unencryptedCard, publicKey);
+    try {
+      const encryptedCard = await AdyenCSE.encryptCard(
+        unencryptedCard,
+        publicKey
+      );
+      console.log(JSON.stringify(encryptedCard));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const tryEncryptBin = async () => {
+    let publicKey = ENVIRONMENT.publicKey;
+    try {
+      const encryptBin = await AdyenCSE.encryptBin(
+        '5454 5454 5454 5454',
+        publicKey
+      );
+      console.log(encryptBin);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
-    <SafeAreaView
-      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-    >
-      <Button onPress={() => tryEncrypt()} title="Encrypt" />
+    <SafeAreaView style={Styles.page}>
+      <Button onPress={() => tryEncryptCard()} title="Encrypt Card" />
+      <Button onPress={() => tryEncryptBin()} title="Encrypt Bin" />
     </SafeAreaView>
   );
 };
