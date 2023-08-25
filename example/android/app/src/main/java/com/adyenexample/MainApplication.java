211a70518eee8acea79d66c0568d2edd5d32a436
Vladimir Abramichev <vladimir.abramichev@adyen.com>
2023-08-25 14:31:32 +0200

    chore: remove direct dependendy

--- example/android/app/src/main/java/com/adyenexample/MainApplication.java
+++ example/android/app/src/main/java/com/adyenexample/MainApplication.java
@@ -27,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-            packages.add(new AdyenPaymentPackage());
           return packages;
         }
 
