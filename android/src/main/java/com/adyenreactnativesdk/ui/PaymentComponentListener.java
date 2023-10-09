598d5b17a37964fedc37f910786ad0bb90258f99
Vladimir Abramichev <vladimir.abramichev@adyen.com>
2023-10-09 19:33:51 +0200

    chore: update to Kotlin

--- android/src/main/java/com/adyenreactnativesdk/ui/PaymentComponentListener.java
+++
@@ -1,8 +0,0 @@
-package com.adyenreactnativesdk.ui;
-
-import com.adyen.checkout.components.model.payments.request.PaymentComponentData;
-
-public interface PaymentComponentListener {
-    void onError(Exception exception);
-    void onSubmit(PaymentComponentData data);
-}
