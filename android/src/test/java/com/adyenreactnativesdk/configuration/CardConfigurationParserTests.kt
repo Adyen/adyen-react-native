831dc62ae11ba6f6802c2be79837a55b771a4b88
Vladimir Abramichev <vladimir.abramichev@adyen.com>
2023-11-06 17:22:10 +0100

    chore: add anroid tests

---
+++ android/src/test/java/com/adyenreactnativesdk/configuration/CardConfigurationParserTests.kt
@@ -0,0 +1,46 @@
+package com.adyenreactnativesdk.configuration
+
+import org.junit.Test
+
+class CardConfigurationParserTest {
+
+    @Test
+    fun getConfiguration() {
+    }
+
+    @Test
+    fun testGetConfiguration() {
+    }
+
+    @Test
+    fun getShowStorePaymentField() {
+    }
+
+    @Test
+    fun getHolderNameRequired() {
+    }
+
+    @Test
+    fun getHideCvcStoredCard() {
+    }
+
+    @Test
+    fun getSupportedCountries() {
+    }
+
+    @Test
+    fun getKcpVisibility() {
+    }
+
+    @Test
+    fun getAddressVisibility() {
+    }
+
+    @Test
+    fun getSupportedCardTypes() {
+    }
+
+    @Test
+    fun getSocialSecurityNumberVisibility() {
+    }
+}
