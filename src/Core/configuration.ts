03c7a1f499c620d71623ebf924db90c59e768af2
Vladimir Abramichev <vladimir.abramichev@adyen.com>
2023-11-07 17:10:59 +0100

    chore: add missing GooglePay type description

--- src/Core/configuration.ts
+++ src/Core/configuration.ts
@@ -107,7 +107,12 @@ export interface ApplePayConfiguration {
 }
 
 /** Collection of values for address field visibility. */
-export type ApplePayAddressFields = 'postalAddress' | 'name' | 'phoneticName' | 'phone' | 'email';
+export type ApplePayAddressFields =
+  | 'postalAddress'
+  | 'name'
+  | 'phoneticName'
+  | 'phone'
+  | 'email';
 
 /** An object that defines a summary item in a payment request—for example, total, tax, discount, or grand total. */
 export interface ApplePaySummaryItem {
@@ -162,6 +167,28 @@ export enum GooglePayEnvironment {
   Production = 1,
 }
 
+export type GooglePayBillingAddressFormat = 'MIN' | 'FULL';
+
+/** An object that defines a summary item in a payment request—for example, total, tax, discount, or grand total. */
+export interface GooglePayBillingAddressParameters {
+  /** Billing address format required to complete the transaction.
+   *
+   * MIN: Name, country code, and postal code (default).
+   * FULL: Name, street address, locality, region, country code, and postal code.
+   */
+  format?: GooglePayBillingAddressFormat;
+  /** Set to true if a phone number is required for the provided shipping address. */
+  phoneNumberRequired?: boolean;
+}
+
+/** An object that defines a summary item in a payment request—for example, total, tax, discount, or grand total. */
+export interface GooglePayShippingAddressParameters {
+  /** ISO 3166-1 alpha-2 country code values of the countries where shipping is allowed. If this object isn't specified, all shipping address countries are allowed. */
+  allowedCountryCodes?: [string];
+  /** Set to true if a phone number is required for the provided shipping address. */
+  phoneNumberRequired?: boolean;
+}
+
 export interface GooglePayConfiguration {
   /**  The merchant account to be put in the payment token from Google to Adyen. By default uses value from brands. */
   merchantAccount?: string;
@@ -175,10 +202,14 @@ export interface GooglePayConfiguration {
   allowPrepaidCards?: boolean;
   /** Set to true if you require a billing address. A billing address should only be requested if it's required to process the transaction. */
   billingAddressRequired?: boolean;
+  /** The expected fields returned if billingAddressRequired is set to true. */
+  billingAddressParameters?: GooglePayBillingAddressParameters;
   /** Set to true to request an email address. */
   emailRequired?: boolean;
   /** Set to true to request a full shipping address. */
   shippingAddressRequired?: boolean;
+  /** The expected fields returned if shippingAddressRequired is set to true. */
+  shippingAddressParameters?: GooglePayShippingAddressParameters;
   /** If set to true then the IsReadyToPayResponse object includes an additional paymentMethodPresent property that describes the visitor's readiness to pay with one or more payment methods specified in allowedPaymentMethods. */
   existingPaymentMethodRequired?: boolean;
   /** The environment to be used by GooglePay. Should be either WalletConstants.ENVIRONMENT_TEST (3) or WalletConstants.ENVIRONMENT_PRODUCTION (1). By default is using environment from root. */
