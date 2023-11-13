bd02c55dee90709566df032196ff03f22f1ed95e
Vladimir Abramichev <vladimir.abramichev@adyen.com>
2023-11-13 10:34:24 +0100

    chore: given-when-then android

--- android/src/test/java/com/adyenreactnativesdk/configuration/GooglePayConfigurationParserTest.kt
+++ android/src/test/java/com/adyenreactnativesdk/configuration/GooglePayConfigurationParserTest.kt
@@ -27,6 +27,7 @@ class GooglePayConfigurationParserTest {
 
     @Test
     fun getAllowedCardNetworks() {
+        // GIVEN
         mockStatic = mockStatic(Log::class.java)
         var wrong_cards_count = 0
         `when`(Log.w(eq("GooglePayConfigParser"), anyString())).thenReturn(wrong_cards_count++)
@@ -34,34 +35,55 @@ class GooglePayConfigurationParserTest {
         val mockArray = mock(ReadableArray::class.java)
         `when`(mockArray.toArrayList()).thenReturn(arrayListOf("MASTERCARD", "visa", "Amex", "wrong_value"))
         config.putArray(GooglePayConfigurationParser.ALLOWED_CARD_NETWORKS_KEY, mockArray)
+
+        // WHEN
         val googlepayParser = GooglePayConfigurationParser(config)
+
+        // THEN
         assertEquals(googlepayParser.allowedCardNetworks, listOf("MASTERCARD", "VISA", "AMEX"))
         assertEquals(wrong_cards_count, 1)
+
+        // TEAR DOWN
         mockStatic?.close()
     }
 
     @Test
     fun getAllowedAuthMethods() {
+        // GIVEN
         val config = WritableMapMock()
         val mockArray = mock(ReadableArray::class.java)
         `when`(mockArray.toArrayList()).thenReturn(arrayListOf("PAN_ONLY", "CRYPTOGRAM_3DS"))
         config.putArray(GooglePayConfigurationParser.ALLOWED_AUTH_METHODS_KEY, mockArray)
+
+        // WHEN
         val googlepayParser = GooglePayConfigurationParser(config)
+
+        // THEN
         assertEquals(googlepayParser.allowedAuthMethods, listOf("PAN_ONLY", "CRYPTOGRAM_3DS"))
     }
 
     @Test
     fun getGooglePayEnvironment() {
+        // GIVEN
         val config = WritableMapMock()
         config.putInt(GooglePayConfigurationParser.GOOGLEPAY_ENVIRONMENT_KEY, 1)
+
+        // WHEN
         val googlepayParser = GooglePayConfigurationParser(config)
+
+        // THEN
         assertEquals(googlepayParser.getGooglePayEnvironment(Environment.TEST), 1 )
     }
 
     @Test
     fun getGooglePayNoEnvironment() {
+        // GIVEN
         val config = WritableMapMock()
+
+        // WHEN
         val googlepayParser = GooglePayConfigurationParser(config)
+
+        // THEN
         assertEquals(googlepayParser.getGooglePayEnvironment(Environment.TEST), 3)
     }
 
