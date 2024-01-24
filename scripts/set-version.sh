f383447071bfeeb60681d6603c98f0121051dd42
vladimir <vladimir.abramichev@adyen.com>
2024-01-24 17:02:33 +0100

    chore: add config parsing tests

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -4,9 +4,8 @@ VERSION=$(cat package.json | python3 -c "import sys, json; print(json.load(sys.s
 echo "Set Version"
 echo $VERSION
 
-
 IOS_PATH="ios/Version.swift"
 sed -i '' "s/{SDK_VERSION}/$VERSION/g" $IOS_PATH
 
-ANDROID_PATH="android/build.gradle"
+ANDROID_PATH="android/dependencies.gradle"
 sed -i '' "s/{SDK_VERSION}/$VERSION/g" $ANDROID_PATH
