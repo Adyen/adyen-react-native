5599e0e16ceed51cf46e3148281102207e40eff1
vladimir <vladimir.abramichev@adyen.com>
2024-03-20 15:52:03 +0100

    break: remove adyenReactNativeRedirectScheme

--- docs/v2-MigrationGuide.md
+++ docs/v2-MigrationGuide.md
@@ -2,10 +2,13 @@
 
 ## Breacking changes
 
+### Android
+
 * Merchant's app theme must be decendent of `Theme.MaterialComponents` to operate with "instant" payment components (ex. Paypal, Klarna). Example:
 ```xml
     <style name="AppTheme" parent="Theme.MaterialComponents.DayNight.NoActionBar"> 
 ```
+* `adyenReactNativeRedirectScheme` was deprecared. Use any [intentFilter](https://developer.android.com/guide/components/intents-filters). SDK will still provides `returnUrl` value inside of the `onSubmit.data` in case it is needed, be cautious to not override it.
 
 ## Non breacking changes
 
--- src/Core/configuration.ts
+++ src/Core/configuration.ts
@@ -12,13 +12,10 @@ export type Environment =
 export interface BaseConfiguration {
   /** Configuration for analytics service */
   analytics?: AnalyticsOptions;
-
   /** Selected environment */
   environment: Environment;
-
   /** A public key linked to your web service user, used for {@link https://docs.adyen.com/user-management/client-side-authentication | client-side authentication}. */
   clientKey: string;
-
   /**
   * The shopper's locale. This is used to enforce the language rendered in the UI.
   * If no value is set, will rely on the system to choose the best fitting locale based on the device's locale and locales supported by the app.
@@ -29,13 +26,9 @@ export interface BaseConfiguration {
 }
 
 export interface AnalyticsOptions {
-  /**
-   * Enable/Disable all telemetry.
-   */
+  /** Enable/Disable all telemetry. */
   enabled?: boolean;
-  /**
-   * Enable/Disable verbose logs printed in the IDE developer console.
-   */
+  /** Enable/Disable verbose logs printed in the IDE developer console. */
   verboseLogs?: boolean;
 }
 
@@ -43,25 +36,18 @@ export interface AnalyticsOptions {
  * General type for AdyenContext configuration. See {@link https://github.com/Adyen/adyen-react-native/blob/develop/docs/Configuration.md}
  */
 export interface Configuration extends BaseConfiguration {
-
-  /** Return URL to be called after payment is completed. */
+  /** Return URL to be called after payment is completed. This value is always passed throught on iOS and in some cases overrided on Android. */
   returnUrl: string;
-
-  /** The shopper's country code. A valid value is an ISO two-character country code (e.g. 'NL'). */
+  /** The shopper's country code. A valid value is an ISO two-character country code (e.g. 'NL'). Required for iOS to visualize the amount. */
   countryCode?: string;
-
   /** Amount to be displayed on the Pay Button. */
   amount?: PaymentAmount;
-
   /** Drop-In configuration. */
   dropin?: DropInConfiguration;
-
   /** Card component configuration. */
   card?: CardsConfiguration;
-
   /** Apple Pay component configuration. */
   applepay?: ApplePayConfiguration;
-
   /** Google Pay component configuration. */
   googlepay?: GooglePayConfiguration;
 }
@@ -69,7 +55,6 @@ export interface Configuration extends BaseConfiguration {
 export interface DropInConfiguration {
   /**  Determines whether to enable preselected stored payment method view step */
   showPreselectedStoredPaymentMethod?: boolean;
-
   /** Determines whether to enable skipping payment list step when there is only one non-instant payment method. */
   skipListWhenSinglePaymentMethod?: boolean;
 }
