cbd4fecc30b656a77300843ac65e674425bbe5bb
vladimir <vladimir.abramichev@adyen.com>
2024-10-04 12:47:45 +0200

    feat: add threeds configuration typesript description

---
+++ src/core/configurations/ThreeDSConfiguration.ts
@@ -0,0 +1,5 @@
+/** 3D Secure 2 authenticationconfiguration. */
+export interface ThreeDSConfiguration {
+  /** Alternative `returnURL` value that could be used for 3D Security 2 OOB flow. */
+  requestorAppUrl?: string;
+}
