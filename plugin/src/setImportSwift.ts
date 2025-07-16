91ae3fcb3af68de23bbea29c5fe7e9a45947d9f9
Vladimir Abramichev <vladimir.abramichev@adyen.com>
2025-07-16 18:10:47 +0200

    chore: add JS code coverage

--- plugin/src/setImportSwift.ts
+++ plugin/src/setImportSwift.ts
@@ -1,10 +1,19 @@
 export function setImportSwift(contents: string): string {
-  const importLine = 'import AdyenReactNative';
+  const importLine = 'import Adyen';
 
   if (contents.includes(importLine)) {
     return contents;
   }
 
-  contents = contents.replace('import Expo', `import Expo\n${importLine}`);
+  const importExpo = 'import Expo';
+  if (contents.includes(importExpo)) {
+    return contents.replace(importExpo, `${importExpo}\n${importLine}`);
+  }
+
+  const importUIKit = 'import UIKit';
+  if (contents.includes(importUIKit)) {
+    return contents.replace(importUIKit, `${importUIKit}\n${importLine}`);
+  }
+
   return contents;
 }
