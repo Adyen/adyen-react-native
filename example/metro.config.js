41e5b6a822f49c97e505e3c48825694ee13b6181
vladimir <vladimir.abramichev@adyen.com>
2025-08-18 15:28:32 +0200

    chore: updating example project

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { getDefaultConfig } = require('@react-native/metro-config');
-const { withMetroConfig } = require('react-native-monorepo-config');
+const { getConfig } = require('react-native-builder-bob/metro-config');
 
 const root = path.resolve(__dirname, '..');
 
@@ -10,7 +10,7 @@ const root = path.resolve(__dirname, '..');
  *
  * @type {import('metro-config').MetroConfig}
  */
-module.exports = withMetroConfig(getDefaultConfig(__dirname), {
+module.exports = getConfig(getDefaultConfig(__dirname), {
   root,
-  dirname: __dirname,
+  project: __dirname,
 });
