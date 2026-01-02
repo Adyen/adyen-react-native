90d82e0c93007c319233bb8bd440018b8768993a
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2026-01-02 14:27:30 +0100

    Add secrets json for example app and add set its reference in Configuration.ts

--- example/src/Configuration.ts
+++ example/src/Configuration.ts
@@ -1,5 +1,6 @@
 import type { Environment } from '@adyen/react-native';
 import { Platform, I18nManager } from 'react-native';
+import secrets from '../secrets.json';
 
 let locale = I18nManager.getConstants().localeIdentifier ?? 'en-US';
 export const DEVICE_LOCALE = locale.replace('_', '-');
@@ -13,8 +14,8 @@ export const DEFAULT_CONFIGURATION = {
   countryCode: 'NL',
   currency: 'EUR',
   amount: 1000, // The amount value in minor units.
-  merchantAccount: '{YOUR_MERCHANT_ACCOUNT}',
-  merchantName: 'MyStore',
+  merchantAccount: secrets.MERCHANT_ACCOUNT, // Or '{YOUR_MERCHANT_ACCOUNT}'
+  merchantName: secrets.merchantName, // Or '{YOUR_MERCHANT_NAME}'
   shopperLocale: DEVICE_LOCALE,
   shopperReference: 'Checkout Shopper',
 };
@@ -22,10 +23,10 @@ export const DEFAULT_CONFIGURATION = {
 // For test purposes only! Do not call Adyen API from your mobile app on LIVE.
 export const ENVIRONMENT = {
   environment: 'test' as Environment,
-  apiKey: '{YOUR_DEMO_SERVER_API_KEY}',
+  apiKey: secrets.X_API_KEY, // Or '{YOUR_DEMO_SERVER_API_KEY}'
   url: 'https://checkout-test.adyen.com/v70/',
-  publicKey: '{YOUR_PUBLIC_KEY}',
-  clientKey: '{YOUR_CLIENT_KEY}',
+  publicKey: secrets.PUBLIC_KEY, // Or '{YOUR_PUBLIC_KEY}'
+  clientKey: secrets.CLIENT_KEY, // Or '{YOUR_CLIENT_KEY}'
   returnUrl: 'myapp://payment', // Only used for iOS
-  applepayMerchantID: '{YOUR_APPLE_MERCHANT_ID}',
+  applepayMerchantID: secrets.APPLE_PAY_MERCHANT_ID_KEY, // Or '{YOUR_APPLE_MERCHANT_ID}'
 };
