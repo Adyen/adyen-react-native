b9df3b3cb1981a9d9f7e61ef4f71027502a28c0d
vladimir <vladimir.abramichev@adyen.com>
2026-01-07 16:03:12 +0100

    chore: simplify barel exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export * from './GooglePayButton';
 export * from './ApplePayButton';
-export { AdyenCheckout } from './AdyenCheckout';
-export type { AdyenCheckoutProps } from './AdyenCheckout';
+export * from './AdyenCheckout';
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,18 +1,7 @@
-export { AdyenApplePay } from './applepay/AdyenApplePay';
-export type { ApplePayModule } from './applepay/AdyenApplePay';
-
-export { AdyenGooglePay } from './googlepay/AdyenGooglePay';
-export type { GooglePayModule } from './googlepay/AdyenGooglePay';
-
-export { AdyenInstant } from './instant/AdyenInstant';
-export type { InstantModule } from './instant/AdyenInstant';
-
-export { AdyenDropIn } from './dropin/AdyenDropIn';
-export type { DropInModule } from './dropin/AdyenDropIn';
-
-export { AdyenCSE } from './cse/AdyenCSEModule';
-export type { AdyenCSEModule } from './cse/AdyenCSEModule';
+export * from './applepay/AdyenApplePay';
+export * from './googlepay/AdyenGooglePay';
+export * from './instant/AdyenInstant';
+export * from './dropin/AdyenDropIn';
+export * from './action/AdyenAction';
+export * from './cse/AdyenCSEModule';
 export * from './cse/types';
-
-export { AdyenAction } from './action/AdyenAction';
-export type { ActionModule } from './action/AdyenAction';
