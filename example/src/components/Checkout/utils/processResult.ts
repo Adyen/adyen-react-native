69766b1f4dbb9bee7beee6a44a1363b9737ae170
vladimir <vladimir.abramichev@adyen.com>
2026-01-07 17:33:07 +0100

    chore: use InteractionManager instead of setTimeout

--- example/src/components/Checkout/utils/processResult.ts
+++ example/src/components/Checkout/utils/processResult.ts
@@ -2,6 +2,7 @@ import type { AdyenComponent } from '@adyen/react-native';
 import { isSuccess } from '../../utilities/isSuccess';
 import type { PageProps } from '../../../State/RootStackParamList';
 import type { PaymentResponse } from '../../../api/types';
+import { InteractionManager } from 'react-native';
 
 export function processResult(
   result: PaymentResponse,
@@ -10,8 +11,8 @@ export function processResult(
 ) {
   const success = isSuccess(result.resultCode);
   nativeComponent.hide(success);
-  setTimeout(() => {
+  InteractionManager.runAfterInteractions(() => {
     navigation?.popToTop();
     navigation?.push('Result', { resultCode: result.resultCode });
-  }, 100);
+  });
 }
