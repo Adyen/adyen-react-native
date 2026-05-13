82ae4bb045d841552df58513365fab0c7397f2b6
vladimir <vladimir.abramichev@adyen.com>
2026-05-13 15:09:46 +0200

    fix: update currentShippingMethods after provideCouponCodeUpdate (COSDK-473)

--- ios/Components/ApplePay/ApplePayModule+Delegates.swift
+++ ios/Components/ApplePay/ApplePayModule+Delegates.swift
@@ -93,6 +93,7 @@ extension ApplePayModule {
         let shippingMethods = parseShippingMethods(dict) ?? currentShippingMethods
         let errors = parseErrors(dict)
         DispatchQueue.main.async {
+            self.currentShippingMethods = shippingMethods
             handler(.init(errors: errors, paymentSummaryItems: summaryItems, shippingMethods: shippingMethods))
         }
     }
