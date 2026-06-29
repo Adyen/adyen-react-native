eb60f0799bd81f39d1d2c086bdd987f9d6f821b3
vladimir <vladimir.abramichev@adyen.com>
2026-06-29 17:17:26 +0200

    chore: add tests to android viewModel

---
+++ android/src/test/java/com/adyenreactnativesdk/component/base/viewmodel/SessionsComponentViewModelTest.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2026 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ */
+
+package com.adyenreactnativesdk.component.base.viewmodel
+
+import com.adyenreactnativesdk.AdyenPaymentPackage
+import com.adyenreactnativesdk.component.base.ModuleException
+import com.adyenreactnativesdk.util.messaging.MessageBus
+import com.adyenreactnativesdk.util.messaging.MockEmitter
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class SessionsComponentViewModelTest {
+  private lateinit var mockEmitter: MockEmitter
+
+  @Before
+  fun setUp() {
+    mockEmitter = MockEmitter()
+    injectMessageBus(MessageBus(mockEmitter))
+  }
+
+  @After
+  fun tearDown() {
+    injectMessageBus(null)
+  }
+
+  @Test
+  fun `cancel sends Canceled exception via messageBus onSessionException`() {
+    // GIVEN
+    val sut = SessionsComponentViewModel<Nothing>()
+
+    // WHEN
+    sut.cancel()
+
+    // THEN
+    assertEquals(1, mockEmitter.errors.size)
+    assertTrue(mockEmitter.errors[0].error is ModuleException.Canceled)
+  }
+
+  /**
+   * Injects [bus] into [AdyenPaymentPackage]'s companion-object backing field via reflection.
+   * The `@Volatile private var _messageBus` compiles to a static field on the outer JVM class.
+   */
+  private fun injectMessageBus(bus: MessageBus?) {
+    val field = AdyenPaymentPackage::class.java.getDeclaredField("_messageBus")
+    field.isAccessible = true
+    field.set(null, bus)
+  }
+}
