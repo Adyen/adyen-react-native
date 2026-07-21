dd439f397178156b1b23ef5281036bd0da60b689
vladimir <vladimir.abramichev@adyen.com>
2026-07-21 22:44:26 +0500

    chore: improve script

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-VERSION=$(python3 -c "import json; print(json.load(open('package.json'))['version'])")
+VERSION=$(node -p "require('./package.json').version")
 mkdir -p lib
 printf "export const adyenSDKVersion = '%s';\n" "$VERSION" > lib/Version.ts
